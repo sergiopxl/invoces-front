f911873bdd5e3801219f3757e53969d0a5a92133
Sergio González <sergio@pixelandbyte.es>
2023-11-09 21:07:06 +0100

    añadidos end point contactos update y post

--- utilities/environment.js
+++ utilities/environment.js
@@ -5,4 +5,5 @@ const globalLoginQuery = "https://ngtropart.pixelandbyte.es/apis/login.php";
 const globalClientsGet = "https://ngtropart.pixelandbyte.es/apis/clients_get.php";
 const globalClientUpdate = "https://ngtropart.pixelandbyte.es/apis/client_update.php";
 const globalClientPost = "https://ngtropart.pixelandbyte.es/apis/client_post.php";
-const globalContactUpdate = "https://ngtropart.pixelandbyte.es/apis/contacts_update.php";
+const globalContactUpdate = "https://ngtropart.pixelandbyte.es/apis/contact_update.php";
+const globalContactPost = "https://ngtropart.pixelandbyte.es/apis/contact_post.php";
